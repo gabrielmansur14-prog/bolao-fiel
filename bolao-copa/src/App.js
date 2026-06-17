3a311fff77c77de182e33375adb506e628f7669b
Gabriel <gabrielmansur14@gmail.com>
2026-06-17 12:00:02 -0300

    Primeiro commit do projeto

---
+++ bolao-copa/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>🏆 Bolão da Copa</h1>
+      <p>Bem vindo ao bolão!</p>
+    </div>
+  );
+}
+
+export default App;
